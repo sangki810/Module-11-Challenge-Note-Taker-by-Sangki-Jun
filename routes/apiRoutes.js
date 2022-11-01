c346bead20e2899e66e429771979b4ed19a1f007
sangki810 <113147507+sangki810@users.noreply.github.com>
2022-11-01 18:04:17 -0400

    finished apiRoutes.js file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,29 @@
 // dependencies
 const router = require("express").Router();
 const store = require("./../db/store")
+
+// route to get notes
+router.get("/notes", function (req, res) {
+    store
+        .getNotes()
+        .then(notes => res.json(notes))
+        .catch(err => res.status(500).json(err))
+});
+
+// post the notes to page
+router.post("/notes", function (req, res) {
+    store
+        .addNote(req.body)
+        .then(notes => res.json(notes))
+        .catch(err => res.status(500).json(err))
+});
+
+// deletes title
+router.delete("/notes/:title", function (req, res) {
+    store  
+        .deleteNotes(req.title)
+        .then(() => res.json({ ok: true }))
+        .catch(err => res.status(500).json(err))
+});
+ 
+module.exports = router;
