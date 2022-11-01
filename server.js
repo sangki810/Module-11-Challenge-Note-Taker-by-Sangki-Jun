11d6dfa6cffceda9ef048f1954e7a0e24725d16b
sangki810 <113147507+sangki810@users.noreply.github.com>
2022-11-01 15:31:09 -0400

    finished server.js file

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// dependencies
 const express = require("express");
 const apiRoutes = require("./routes/apiRoutes.js");
 const htmlRoutes = require("./routes/htmlRoutes.js")
@@ -8,15 +9,17 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // set up express to handle data parsing
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // first middleware
 app.use(express.static("public"));
+// use apiRoutes.js file for routes
 app.use("/api", apiRoutes);
+// use htmlRoutes.js file for routes
 app.use("/", htmlRoutes);
 
-
+// server listening for requests
 app.listen(PORT, function () {
     console.log(`App listening at http://localhost:${PORT}`);
 });
