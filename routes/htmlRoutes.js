9ffd5f725edbd6d0d036fd1737da85717c390e4e
sangki810 <113147507+sangki810@users.noreply.github.com>
2022-11-01 21:34:23 -0400

    added htmlROutes.js content

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,13 +1,17 @@
 // dependencies
 const path = require("path");
-const router = require("express").Router();
+const express = require("express");
+const app = express();
+const router = express.Router();
 
+// gets notes.html
 router.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
-
+// gets index.html
 router.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-module.exports = router;
+// exports for other files
+exports.router1 = router;
